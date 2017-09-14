ea1584dcdde2426069fe01a4a890f96b432013c2
Pavel Zaytsev <pzaytsev@Pavel-Zaytsevs-MacBook-Pro.local>
2017-09-14 12:36:02 -0700

    Initial Commit

---
+++ Membership Protocol/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Membership Protocol
+//
+//  Created by Pavel Zaytsev on 9/14/17.
+//  Copyright © 2017 Pavel Zaytsev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
